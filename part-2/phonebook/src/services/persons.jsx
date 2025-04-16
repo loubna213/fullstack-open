ece2c2e9fb747d4880200b5869485439836458e0
loubna213 <loub60418@gmail.com>
2025-04-16 17:20:31 +0100

    phonebook backend step 9

--- part-2/phonebook/src/services/persons.jsx
+++ part-2/phonebook/src/services/persons.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "http://localhost:8000/api/persons/"
 
 export const getAll = () => {
     const request = axios.get(baseUrl)
@@ -13,11 +13,11 @@ export const addPerson = (newObj) => {
 }
 
 export const deletePerson = (id) => {
-    const request = axios.delete(`http://localhost:3001/persons/${id}`)
+    const request = axios.delete(`${baseUrl}${id}`)
     return request.then(res => res.data)
 }
 
 export const updatePerson = (id, newObj) => {
-    const request = axios.put(`http://localhost:3001/persons/${id}`, newObj)
+    const request = axios.put(`${baseUrl}${id}`, newObj)
     return request.then(res => res.data) 
 }
--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 
 
 let persons = [
